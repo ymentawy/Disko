b69d89f449902c3c690dbe50ef919382d06c8885
youssifabuzied <youssif.abuzied@aucegypt.edu>
2023-11-24 23:24:00 +0200

    Added Support for filtering items

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ use chrono::{DateTime, Local};
 struct Configurations {
     is_file: bool,
     max_depth: usize,
+    include_hidden_files: bool,
 }
 
 #[derive(Debug)]
+#[derive(Clone)]
 struct DiskItem {
     name: String,
     is_file: bool,
@@ -39,7 +41,7 @@ fn format_system_time(st: Option<SystemTime>) -> Option<String> {
     })
 }
 
-fn scan_directory(path: &Path, depth: usize, configs: &Configurations) -> io::Result<DiskItem> {
+fn scan_directory(path: &Path, depth: usize) -> io::Result<DiskItem> {
     let metadata = fs::metadata(path)?;
     let path_str = path.to_string_lossy().into_owned();
 
@@ -56,35 +58,87 @@ fn scan_directory(path: &Path, depth: usize, configs: &Configurations) -> io::Re
     };
 
     if metadata.is_dir() {
-        if (depth+1) < configs.max_depth {
-            if let Ok(entries) = fs::read_dir(path) {
-                for entry in entries {
-                    if let Ok(entry) = entry {
-                        let child = scan_directory(&entry.path(), depth + 1, configs)?;
-                        if (configs.is_file && child.is_file) {
-                            item.children.push(child);
-                        }
-                    }
+        if let Ok(entries) = fs::read_dir(path) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let child = scan_directory(&entry.path(), depth + 1)?;
+                    item.children.push(child);
                 }
-                item.size = calculate_disk_usage(&item);
             }
+            item.size = calculate_disk_usage(&item);
         }
     }
 
     Ok(item)
 }
+fn check_file(item: &DiskItem, configs: &Configurations) -> bool {
+    let mut res: bool = false;
+    let mut count:u64 = 0;
+    if(!configs.is_file){
+        res = true;
+    }
+    if(!configs.include_hidden_files){
+        for c in item.name.chars() { 
+            if c == '.' && count == 0{
+                res = true;
+                count = count +1;
+            }
+        }
+    }
+    return !res;
+}
+
+fn filter_items(item: &DiskItem, configs: &Configurations) -> DiskItem {
+    let mut filtered_item = DiskItem {
+        name: item.name.clone(),
+        is_file: item.is_file,
+        size: item.size,
+        last_accessed: item.last_accessed.clone(),
+        last_modified: item.last_modified.clone(),
+        created: item.created.clone(),
+        depth: item.depth,
+        path: item.path.clone(),
+        children: Vec::new(),
+    };
+ 
+    if(!filtered_item.is_file){
+        if(configs.max_depth > filtered_item.depth){
+            // filtered_item.children = item.children
+            // .iter()
+            // .map(|child| filter_items(child, configs))
+            // .collect();
+            for child in item.children.iter(){
+                if(!child.is_file){
+                    filtered_item.children.push(filter_items(child, configs));
+                }else{
+                    if(check_file(child, configs)){
+                        filtered_item.children.push(child.clone());
+                    }
+                }
+
+
+            }
+        }
+    }
+
+
+
+
+    filtered_item
+}
 
 fn main() {
-    let directory_path = Path::new("/home/youssif-abuzied/ana_disk/src");
+    let directory_path = Path::new("/home/youssif-abuzied/Desktop");
     let configs = Configurations {
-        is_file: true, // Change this to true if only files are to be included
-        max_depth: 1,   // Change this depth limit as needed
+        is_file: true, // Set to false to display only folders, true for both files and folders
+        max_depth: 1,
+        include_hidden_files : true,   // Adjust depth as needed
     };
 
-    match scan_directory(&directory_path, 0, &configs) {
-        Ok(result) => {
-            // Printing the result
-            println!("{:#?}", result);
+    match scan_directory(&directory_path, 0) {
+        Ok(scanned_result) => {
+            let filtered_result = filter_items(&scanned_result, &configs);
+            println!("{:#?}", filtered_result);
         }
         Err(e) => eprintln!("Error: {}", e),
     }
